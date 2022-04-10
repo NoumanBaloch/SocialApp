55c8f426af002da4e9e631930df9b5326831a2e5
Nouman Baloch <nomibaloch775@hotmail.com>
2022-04-10 15:20:04 +0500

    Client Skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +59,7 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
