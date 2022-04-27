31631b013d76ffceb38ccaf97d42157d56a7b2f1
Nouman Baloch <nomibaloch775@hotmail.com>
2022-04-27 22:05:29 +0500

    Extending the api

---
+++ API/Helper/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
