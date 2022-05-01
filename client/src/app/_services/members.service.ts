0373e7605045a6e6a97035516726a1457fce145b
Nouman Baloch <nomibaloch775@hotmail.com>
2022-05-01 06:03:44 +0500

    building user interface

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+
+  baseUrl = environment.apiUrl;
+  constructor(private _httpClient: HttpClient) { }
+
+  getMembers(){
+    return this._httpClient.get<Member[]>(this.baseUrl + 'users');
+  }
+
+  getMember(username: string){
+    return this._httpClient.get<Member>(this.baseUrl + 'users/' + username);
+  }
+}
